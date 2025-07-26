3bd78ef7821bccec15b286ef4d298afee4df5461
cryptonocry <132585712+cryptonocry@users.noreply.github.com>
2025-07-26 21:58:08 +0200

    Update digit.js

--- digit.js
+++ digit.js
@@ -1,7 +1,8 @@
+// digit.js
 export class Icon {
   static shakeFactor = 1;
 
-  constructor(gx, gy, type, spawnTime) {
+constructor(gx, gy, type, spawnTime) {
     this.gx = gx;
     this.gy = gy;
     this.type = type;
@@ -9,17 +10,16 @@ export class Icon {
     this.phaseOffset = Math.random() * Math.PI * 2;
     const baseAmp = 3, baseSpd = 0.5;
     this.baseAmplitude = baseAmp * 1.5;
-    this.baseSpeed = baseSpd * 1.2;
+    this.baseSpeed     = baseSpd * 1.2;
     if (!Icon.images) Icon._loadImages();
   }
 
   static _loadImages() {
     Icon.images = {};
     [
-      "mask", "letterS", "cd", "xicon",
-      "clock", "key", "ethicon", "btcicon",
-      "eye", "lock", "scroll", "dna", "flask",
-      "per0", "per20", "per40", "per60", "per80", "per100", "perplus"
+      "mask","letterS","cd","xicon",
+      "clock","key","ethicon","btcicon",
+      "eye","lock","scroll","dna","flask"
     ].forEach(name => {
       const img = new Image();
       img.src = `icons/${name}.svg`;
@@ -45,13 +45,8 @@ export class Icon {
     const SIZE = 30;
     ctx.save();
     ctx.globalAlpha = alpha;
-    ctx.drawImage(img, pos.x - SIZE / 2, pos.y - SIZE / 2, SIZE, SIZE);
-    
-    // Применяем белый цвет поверх иконки
-    ctx.globalCompositeOperation = 'source-atop';
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fillRect(pos.x - SIZE / 2, pos.y - SIZE / 2, SIZE, SIZE);
-    ctx.globalCompositeOperation = 'source-over'; // Возвращаем стандартный режим
+    ctx.drawImage(img, pos.x - SIZE/2, pos.y - SIZE/2, SIZE, SIZE);
     ctx.restore();
   }
 }
+
